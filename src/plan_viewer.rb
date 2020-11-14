d9d5d7ba9afe83cfd7f38b42d94d0b950bfb680d
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2020-11-14 09:18:07 +0800

    pretiffy logger

--- src/plan_viewer.rb
+++ src/plan_viewer.rb
@@ -11,10 +11,23 @@ module PlanViewer
       plans.each {|pl| print_plan(pl)}
     end
 
-    def print_plan plan
-      plan.each_with_index do |station|
-        puts "#{station.name}(#{station.station_code})"
+    def print_plan stations
+      puts "Station Travelled: #{stations.count - 1}"
+      puts "Route: #{stations.map(&:station_code).join(' -> ')}"
+      puts ""
+      puts "Instructions:"
+
+      stations.each_with_index do |st, idx|
+        next_st = stations[idx + 1]
+        next unless next_st
+
+        if st.line_code == next_st.line_code
+          puts "Take #{st.line_code} from #{st.name}(#{st.station_code}) to #{next_st.name}(#{next_st.station_code})"
+        else
+          puts "Change from #{st.line_code} to #{next_st.line_code}"
+        end
       end
+      puts "Arrived!! 🥳🥳🥳"
     end
     private :print_plan
   end
