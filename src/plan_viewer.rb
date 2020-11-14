0e76ef8a95efb42767d220c3ad3b9804c7a5a14b
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2020-11-14 16:16:31 +0800

    add error handling for the given time

--- src/plan_viewer.rb
+++ src/plan_viewer.rb
@@ -1,14 +1,15 @@
 module PlanViewer
   NOT_FOUND_MSG = "No suitable route can be found. 😢😢"
   class << self
-    def print_time_routes results
+    def print_time_routes results, current_time
       if results.empty?
         puts NOT_FOUND_MSG
         return
       end
 
+      puts "Travelled At: #{current_time}"
       results.each do |res|
-        puts "Time: #{res[:time]}"
+        puts "Time Spent: #{res[:time]} minutes"
         print_plan(res[:plan])
       end
     end
